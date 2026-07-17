55d0f7cc8f25a411d69c3e55fbef71720d3430a3
dansc <dansc@yandex.ru>
2026-07-17 20:43:18 +0300

    Tidy: drop the blank line left by the FreeDroidWarn import removal

--- app/src/main/java/com/whispertflite/MainActivity.java
+++ app/src/main/java/com/whispertflite/MainActivity.java
@@ -54,7 +54,6 @@ import com.whispertflite.utils.InputLang;
 import com.whispertflite.utils.LanguagePairAdapter;
 import com.whispertflite.utils.ThemeUtils;
 
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
